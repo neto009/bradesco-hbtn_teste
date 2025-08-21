4087d540fa0045c560fc2963a01f9d8f7f6d7ad6
Solange <solange_freitas01@hotmail.com>
2025-08-21 19:29:49 -0300

    Testes

---
+++ 0x01/mei/src/Person.java
@@ -0,0 +1,113 @@
+import java.util.Calendar;
+import java.util.Date;
+
+public class Person {
+    private String name;
+    private String surname;
+    private Date birthDate;
+    private boolean anotherCompanyOwner;
+    private boolean pensioner;
+    private boolean publicServer;
+    private float salary;
+
+    public Person(String name, String surname, Date birthDate, boolean anotherCompanyOwner, 
+                  boolean pensioner, boolean publicServer) {
+        this.name = name;
+        this.surname = surname;
+        this.birthDate = birthDate;
+        this.anotherCompanyOwner = anotherCompanyOwner;
+        this.pensioner = pensioner;
+        this.publicServer = publicServer;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public boolean isAnotherCompanyOwner() {
+        return anotherCompanyOwner;
+    }
+
+    public void setAnotherCompanyOwner(boolean anotherCompanyOwner) {
+        this.anotherCompanyOwner = anotherCompanyOwner;
+    }
+
+    public boolean isPensioner() {
+        return pensioner;
+    }
+
+    public void setPensioner(boolean pensioner) {
+        this.pensioner = pensioner;
+    }
+
+    public boolean isPublicServer() {
+        return publicServer;
+    }
+
+    public void setPublicServer(boolean publicServer) {
+        this.publicServer = publicServer;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    public String fullName() {
+        return name + " " + surname;
+    }
+
+    public float calculateYearlySalary() {
+        return salary * 12;
+    }
+
+    private int getAge() {
+        if (birthDate == null) {
+            return 0;
+        }
+        
+        Calendar birthCalendar = Calendar.getInstance();
+        birthCalendar.setTime(birthDate);
+        
+        Calendar currentCalendar = Calendar.getInstance();
+        
+        int age = currentCalendar.get(Calendar.YEAR) - birthCalendar.get(Calendar.YEAR);
+
+        if (currentCalendar.get(Calendar.DAY_OF_YEAR) < birthCalendar.get(Calendar.DAY_OF_YEAR)) {
+            age--;
+        }
+        
+        return age;
+    }
+
+    public boolean isMEI() {
+        return (calculateYearlySalary() < 130000) &&
+            (getAge() > 18) &&
+            (!anotherCompanyOwner) &&
+            (!pensioner) &&
+            (!publicServer);
+    }
+}
---
+++ 0x01/mei/src/PersonTest.java
@@ -0,0 +1,45 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Date;
+import java.util.Calendar;
+
+public class PersonTest {
+    private Person person;
+
+    @BeforeEach
+    public void setup() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2000, Calendar.JANUARY, 1);
+        Date birthDate = calendar.getTime();
+        
+        person = new Person("Paul", "McCartney", birthDate, true, true, true);
+    }
+
+    @Test
+    public void show_full_name() {
+        assertEquals("Paul McCartney", person.fullName());
+    }
+
+    @Test
+    public void test_calculateYearlySalary() {
+        person.setSalary(1200);
+        assertEquals(14400, person.calculateYearlySalary(), 0.01);
+    }
+
+    @Test
+    public void person_is_MEI() {
+        person.setSalary(5000);
+        person.setAnotherCompanyOwner(false);
+        person.setPensioner(false);
+        person.setPublicServer(false);
+        
+        assertTrue(person.isMEI());
+    }
+
+    @Test
+    public void person_is_not_MEI() {
+        person.setSalary(15000);
+        assertFalse(person.isMEI());
+    }
+}
